0658a1183e40cccf3a4dce7e562cb9ea094560a4
adityasharma2408 <adityaasharma2408@gmail.com>
2026-03-24 01:09:12 +0530

    palendrome in java

---
+++ palendrome.java
@@ -0,0 +1,19 @@
+package you.strings;
+
+public class palendrome {
+    public static void main (String[] args){
+    String s = "abcdcbba";
+        System.out.println(isPalendrome(s));
+    }
+    static boolean isPalendrome(String s){
+        s=s.toLowerCase();
+        for (int i = 0;i<s.length()/2;i++){
+            int start = s.charAt(i);
+            int end = s.charAt(s.length()-1-i);
+            if (start!=end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
